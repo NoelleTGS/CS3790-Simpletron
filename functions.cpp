b0577234e34101eeeda78543499efc9d65f25d39
Noelle Zahara <noellezahara@gmail.com>
2024-10-01 11:12:38 -0600

    Implement LOAD opcodes

--- functions.cpp
+++ functions.cpp
@@ -5,27 +5,27 @@
 using namespace std;
 
 void dumpCore(const int acc, const int instCount, const int instReg, const int inReg, int (&memory)[PAGES][ROWS][COLUMNS], const int startPage, const int endPage) {
-    printf("%-22s%06d\n", "accumulator", acc);
-    printf("%-22s%06d\n", "instructionCounter", instCount);
-    printf("%-22s%06d\n", "indexRegister", inReg);
-    printf("%-26s%02d\n", "operationCode", instReg / 10000);
-    printf("%-24s%04d\n", "operand", instReg % 1000);
-    cout << endl << "Memory" << endl << "  ";
-    for (int i = startPage; i <= endPage; i++) {
-        printf("\nPage #%d\n", i);
-        cout << "  ";
-        for (int a = 0; a < COLUMNS; a++) {
-            printf(" %6d", a);
-        }
-        cout << endl;
-        for (int j = 0; j < ROWS; j++) {
-            printf("%2d ", j * ROWS);
-            for (int k = 0; k < COLUMNS; k++) {
-                printf("%06d ", memory[i][j][k]);
-            }
-            cout << endl;
-        }
-    }
+	printf("%-22s%06d\n", "accumulator", acc);
+	printf("%-22s%06d\n", "instructionCounter", instCount);
+	printf("%-22s%06d\n", "indexRegister", inReg);
+	printf("%-26s%02d\n", "operationCode", instReg / 10000);
+	printf("%-24s%04d\n", "operand", instReg % 1000);
+	cout << endl << "Memory" << endl << "  ";
+	for (int i = startPage; i <= endPage; i++) {
+		printf("\nPage #%d\n", i);
+		cout << "  ";
+		for (int a = 0; a < COLUMNS; a++) {
+			printf(" %6d", a);
+		}
+		cout << endl;
+		for (int j = 0; j < ROWS; j++) {
+			printf("%2d ", j * ROWS);
+			for (int k = 0; k < COLUMNS; k++) {
+				printf("%06d ", memory[i][j][k]);
+			}
+			cout << endl;
+		}
+	}
 }
 
 // executeInstruction function
@@ -33,28 +33,34 @@ void dumpCore(const int acc, const int instCount, const int instReg, const int i
 int executeInstruction(int &acc, int &instCount, int &instReg, int &inReg, int (&memory)[PAGES][ROWS][COLUMNS]) {
 
 	//Separate the instructionRegister into the operation code (first two digits) and the operand (last four digits)
-    const int opCode = instReg / 10000;
-    const int operand = instReg % 10000;
+	const int opCode = instReg / 10000;
+	const int operand = instReg % 10000;
 
-    int page = operand / 100;
-    int row = (operand % 100) / 10;
-    int column = operand % 10;
+	int page = operand / 100;
+	int row = (operand % 100) / 10;
+	int column = operand % 10;
 
-    switch(opCode) {
-        case READ:						// Read from terminal into memory
-            int input;
-            cout << "Input: ";
-            cin >> input;
-            memory[page][row][column] = input;
-            break;
-        case WRITE:						// Write from memory to terminal
-            cout << memory[page][row][column] << endl;
-            break;
-        case HALT:						// End execution of program
-            dumpCore(acc, instCount, instReg, inReg, memory, 0, 0);
-            return 1;
-        default: break;
-    }
-    instCount++;
-    return 0;
+	switch(opCode) {
+		case READ:						// Read from terminal into memory
+			int input;
+			cout << "Input: ";
+			cin >> input;
+			memory[page][row][column] = input;
+			break;
+		case WRITE:						// Write from memory to terminal
+			cout << memory[page][row][column] << endl;
+			break;
+		case LOAD:						// Load from memory to accumulator
+			acc = memory[page][row][column];
+			break;
+		case LOADIM:					// Load the operand into accumulator
+			acc = operand;
+			break;
+		case HALT:						// End execution of program
+			dumpCore(acc, instCount, instReg, inReg, memory, 0, 0);
+			return 1;
+		default: break;
+	}
+	instCount++;
+	return 0;
 }
